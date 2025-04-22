9ca7e7ea764941fa5a7bd24c198fe3fae172f86d
Avneesh Dayal <avneeshd09@gmail.com>
2025-04-22 23:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const menuIcon = document.getElementById('menu-icon');
- const navbar = document.querySelector('.navbar');
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.getElementById("navbar");
 
-
- menuIcon.addEventListener('click', () => {
-     navbar.classList.toggle('active');
- });
+menuIcon.onclick = () => {
+    navbar.classList.toggle("active");
+};
