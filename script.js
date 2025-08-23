a77bb1732eca01292efdbd98261673b384fd96ba
Avneesh Dayal <avneeshd09@gmail.com>
2025-08-23 23:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const menuIcon = document.getElementById("menu-icon");
-const navbar = document.getElementById("navbar");
+<script>
+    let menuIcon = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () => {
-    navbar.classList.toggle("active");
-};
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle("bx-x"); 
+        navbar.classList.toggle("active");
+    };
+
+    document.querySelectorAll(".navbar a").forEach(link => {
+        link.onclick = () => {
+            menuIcon.classList.remove("bx-x");
+            navbar.classList.remove("active");
+        };
+    });
+</script>
